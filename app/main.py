fa0e96bfe466886cafbd1ecbcc3720d0b8195358
Oleksii-Hrechukha <Oleksii_Hrechukha@outlook.com>
2025-03-29 13:19:18 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def longtime_function(*args: Any, **kwargs: Any) -> Any:
+        immutable_types = (int, float, str, bool, tuple)
+
+        for arg in args:
+            if isinstance(arg, immutable_types):
+                continue
+            else:
+                print("This is mutable!")
+                break
+
+        if args not in cache_dict.keys():
+            print("Calculating new result")
+            result_function = func(*args, **kwargs)
+            cache_dict[args] = result_function
+            return result_function
+        else:
+            print("Getting from cache")
+            return cache_dict[args]
+    return longtime_function
