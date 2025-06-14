0a20328a9d504ba1c5767f00d75fdd99cba3cc5f
Oleksii-Hrechukha <Oleksii_Hrechukha@outlook.com>
2025-06-14 15:23:29 +0300

    new_solution

--- app/main.py
+++ app/main.py
@@ -4,21 +4,34 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def longtime_function(*args: Any) -> Any:
+    def longtime_function(*args: Any, **kwargs: Any) -> Any:
 
         for arg in args:
             if isinstance(arg, (int, float, str, bool, tuple)):
-                continue
+                if args not in cache_dict:
+                    print("Calculating new result")
+                    result_function = func(*args)
+                    cache_dict[args] = result_function
+                    return result_function
+                else:
+                    print("Getting from cache")
+                    return cache_dict[args]
             else:
                 print("This is mutable!")
                 break
 
-        if args not in cache_dict.keys():
-            print("Calculating new result")
-            result_function = func(*args)
-            cache_dict[*args] = result_function
-            return result_function
-        else:
-            print("Getting from cache")
-            return cache_dict[args]
+        for kwarg in kwargs:
+            if isinstance(kwarg, (int, float, str, bool, tuple)):
+                if kwargs not in cache_dict:
+                    print("Calculating new result")
+                    result_function = func(**kwargs)
+                    cache_dict[kwargs] = result_function
+                    return result_function
+                else:
+                    print("Getting from cache")
+                    return cache_dict[kwargs]
+            else:
+                print("This is mutable!")
+                break
+
     return longtime_function
